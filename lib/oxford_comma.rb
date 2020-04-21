331e850cb000c61cd2b940f4aeaa8a8e71436a08
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-21 21:43:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 names = ["ash"]
 def oxford_comma(array)
-case array
-when array.length == 1
-  final = array.join 
+  case array
+    when array.length == 1
+        final = array.join 
+      else 
+        final
+  end
 end
+
+
