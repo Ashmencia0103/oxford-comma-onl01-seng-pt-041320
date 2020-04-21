eef872d9c00e0a84bf2962cb761964e9da106ebe
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-21 21:55:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
-name = ["ash"]
+name = ["ash","ale"]
 def oxford_comma(array)
   if array.length == 1
     final = array.join
+  elsif array.length ==2
+    final = array.join (" and ")
+  end
+
   end
 end
 oxford_comma(name)
