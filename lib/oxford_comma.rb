f5635c15135b2a9575e0582ffc129215e97dbd44
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-21 21:57:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ name = ["ash","ale"]
 def oxford_comma(array)
   if array.length == 1
     final = array.join
-  elsif array.length ==2
-    final = array.join (" and ")
-  end
-
+  elsif array.length == 2
+    final = array.join (' and ')
+  else
+    final
   end
 end
 oxford_comma(name)
